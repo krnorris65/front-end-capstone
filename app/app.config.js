d2e254097a9b98242758f221ca69d868e3509289
krnorris65 <32465587+krnorris65@users.noreply.github.com>
2017-12-06 11:49:26 -0600

    Delete app.config.js

--- app/app.config.js
+++
@@ -1,9 +0,0 @@
-angular.module("LifeReelApp").constant("FIREBASE_CONFIG", {
-	apiKey: "AIzaSyBSmCGgrcgzzSjDzc6stzPdwaFR5agGkfs",
-	authDomain: "life-reel.firebaseapp.com",
-	databaseURL: "https://life-reel.firebaseio.com",
-	projectId: "life-reel",
-	storageBucket: "",
-	messagingSenderId: "81694446627"
-})
-
