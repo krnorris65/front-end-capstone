21e8df00511004cb80cfd546d6dc3a6089ec84af
Kristen Norris <krnorris65@gmail.com>
2017-12-07 15:20:19 -0600

    starting event form

--- app/events/controllers/LandingCtrl.js
+++ app/events/controllers/LandingCtrl.js
@@ -1,13 +1,12 @@
 angular.module("LifeReelApp")
 	.controller("LandingCtrl", function($scope, UserFactory, $timeout){
 
-        // welcome message
-        // $scope.heading = ""
         
         $timeout( ()=> {
             // gets the current user from the database
             UserFactory.currentUser().then(user => {
-               
+                
+                // welcome message
                 const firstName = user.first
                 $timeout()
                 $scope.heading = `Welcome ${firstName}!`
