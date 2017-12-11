0730e4d8c413c3b0e2684307604a2f67f6c7df84
Kristen Norris <krnorris65@gmail.com>
2017-12-11 10:17:19 -0600

    proper rating is selected when you edit the event

--- app/events/controllers/EditEventCtrl.js
+++ app/events/controllers/EditEventCtrl.js
@@ -2,10 +2,12 @@ angular.module("LifeReelApp")
     .controller("EditEventCtrl", function($scope, $routeParams, EventFactory, $timeout){
 
         $scope.event = {}
+        $scope.selectedRating = 0
 
         EventFactory.single($routeParams.eventId).then (event => {
             $timeout( () => {
                 $scope.event= event
+                $scope.selectedRating = event.rating
 
             }, 100)
         })
