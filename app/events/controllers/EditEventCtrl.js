3573bae8fc17b8cc724c3a258fbc62ac7cf4c5a4
Kristen Norris <krnorris65@gmail.com>
2017-12-11 11:25:41 -0600

    added cancel button and made sure that edit form is working properly before linking to firebase

--- app/events/controllers/EditEventCtrl.js
+++ app/events/controllers/EditEventCtrl.js
@@ -1,17 +1,30 @@
 angular.module("LifeReelApp")
-    .controller("EditEventCtrl", function($scope, $routeParams, EventFactory, $timeout){
+    .controller("EditEventCtrl", function($scope, $routeParams, EventFactory, $timeout, $location){
 
-        $scope.event = {}
+        $scope.editEvent = {}
         $scope.selectedRating = 0
+        
 
         EventFactory.single($routeParams.eventId).then (event => {
             $timeout( () => {
-                $scope.event= event
+                $scope.editEvent= event
                 $scope.selectedRating = event.rating
 
             }, 100)
         })
 
+        $scope.saveEvent = (event) => {
 
+            if($scope.date !== undefined) {
+                event.date = Date.parse($scope.date)
+            }
+            
+            event.rating = parseInt($scope.selectedRating)
+            console.log(event)
+        }
+
+        $scope.cancelEdit = () => {
+            $location.url("/reel")
+        }
 
     })
