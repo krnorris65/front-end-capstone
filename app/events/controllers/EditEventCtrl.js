0d04ad094e7f946fef49ad5c54a1331281670b8f
Kristen Norris <krnorris65@gmail.com>
2017-12-11 11:37:07 -0600

    user can successfully edit their event

--- app/events/controllers/EditEventCtrl.js
+++ app/events/controllers/EditEventCtrl.js
@@ -1,30 +1,39 @@
 angular.module("LifeReelApp")
     .controller("EditEventCtrl", function($scope, $routeParams, EventFactory, $timeout, $location){
 
-        $scope.editEvent = {}
-        $scope.selectedRating = 0
+        $scope.editEvent = {} 
+        $scope.selectedRating = 0 
         
 
         EventFactory.single($routeParams.eventId).then (event => {
             $timeout( () => {
-                $scope.editEvent= event
-                $scope.selectedRating = event.rating
+                $scope.editEvent= event //event information to edit
+                $scope.selectedRating = event.rating //value of rating for radio button to show as selected
 
             }, 100)
         })
 
-        $scope.saveEvent = (event) => {
+        //brings user back to their reel if they decide they don't want to make any changes to the event
+        $scope.backToReel = () => {
+            $location.url("/reel")
+        }
 
+        
+        $scope.saveEvent = (event) => {
+            //if no changes were made to the date, then don't reset the value of the date key. if changes were made, then parse the date and reset the value of the date key
             if($scope.date !== undefined) {
                 event.date = Date.parse($scope.date)
             }
             
-            event.rating = parseInt($scope.selectedRating)
-            console.log(event)
+            event.rating = parseInt($scope.selectedRating) //parses the value string to a number
+
+            //updates firebase with edits made and brings user back to their reel
+            EventFactory.edit($routeParams.eventId, event).then( reload => {
+                $timeout( ()=> {
+                    $scope.backToReel()
+                }, 100)
+            })
         }
 
-        $scope.cancelEdit = () => {
-            $location.url("/reel")
-        }
 
     })
