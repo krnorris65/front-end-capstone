ccd4442596e0af019597a038e96706341157cc46
Kristen Norris <krnorris65@gmail.com>
2017-12-12 17:21:50 -0600

    Show Ctrl interates through the sentCache and receivedCache and adds any friends who have a pending as false to an allFriends array

--- app/friends/controllers/ShowCtrl.js
+++ app/friends/controllers/ShowCtrl.js
@@ -1,18 +1,45 @@
 angular.module("LifeReelApp")
     .controller("ShowCtrl", function($scope, FriendsFactory, $timeout){
 
+        $scope.allFriends = []
+
         $scope.heading = "Added Friends"
-        $timeout(() => {
-
-            $scope.sent = FriendsFactory.sentCache.filter(friend => {
-                return friend.pending === false
-            })
-            $scope.received = FriendsFactory.receivedCache.filter(friend => {
-                return friend.pending === false
-            })
-            console.log("sent", $scope.sent)
-            console.log("received", $scope.received)
+        $timeout(()=>{
+            if(FriendsFactory.sentCache !== null || FriendsFactory.receivedCache !== null) {
+                if(FriendsFactory.sentCache.length > 0){
+                    FriendsFactory.sentCache.forEach(friend => {
+                        if(friend.pending === false) {
+                            let friendObj = {
+                                "friendName": friend.receiverName,
+                                "friendUID": friend.receiverUID,
+                                "friendKey": friend.friendID,
+                                "date": friend.date
+                            }
+                            $scope.allFriends.push(friendObj)
+                        }
+                    })
+                }
+    
             
+                if(FriendsFactory.receivedCache.length > 0) {
+                    FriendsFactory.receivedCache.forEach(friend => {
+                        if(friend.pending === false) {
+                            let friendObj = {
+                                "friendName": friend.senderName,
+                                "friendUID": friend.senderUID,
+                                "friendKey": friend.friendId,
+                                "date": friend.date
+                            }
+                            $scope.allFriends.push(friendObj)
+                        }
+                    })
+                }
+            }
 
+
+    
+        console.log($scope.allFriends)
         }, 500)
+
+            
     })
