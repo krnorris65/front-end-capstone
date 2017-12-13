a7694853c732e0fff8f620fe8e83a79d7852de6a
Kristen Norris <krnorris65@gmail.com>
2017-12-13 11:16:14 -0600

    can view your friends reels

--- app/friends/controllers/FriendReelCtrl.js
+++ app/friends/controllers/FriendReelCtrl.js
@@ -1,10 +1,16 @@
 angular.module("LifeReelApp")
-    .controller("FriendReelCtrl", function($scope, EventFactory, $routeParams){
+    .controller("FriendReelCtrl", function($scope, EventFactory, $routeParams, $timeout){
 
         $scope.heading = "Friend's Reel"
-        // EventFactory.friendEvents($routeParams.friendId).then(event => {
-        //     debugger
-        // })
+
+        EventFactory.friendEvents($routeParams.friendId).then(events => {
+            $timeout(() => {
+                $scope.eventArray = events.filter(event => {
+                    return event.private === false
+                })
+
+            }, 100)
+        })
 
 
     })
