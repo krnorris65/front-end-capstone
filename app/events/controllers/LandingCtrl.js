d8344989ffcd21c2eadaea7959c5102aaa9e7358
Kristen Norris <krnorris65@gmail.com>
2017-12-29 15:24:07 -0600

    formatted landing page

--- app/events/controllers/LandingCtrl.js
+++ app/events/controllers/LandingCtrl.js
@@ -2,21 +2,15 @@ angular.module("LifeReelApp")
 	.controller("LandingCtrl", function($scope, UserFactory, $timeout, $location, EventFactory){
         //loads all users
         UserFactory.allUsers()
-
-        $timeout( ()=> {
-            // gets the current user from the database
-            UserFactory.currentUser().then(user => {
-                
-                // welcome message
-                const firstName = user.first
-                $timeout()
-                $scope.heading = `Welcome ${firstName}!`
-                $scope.addButton = "Add New Segment"
-            })
-        }, 500)
+        // gets the current user from the database
+        UserFactory.currentUser()
 
         $scope.eventForm = () => {
             $location.url("/add")
+        }
+
+        $scope.viewReel = () => {
+            $location.url("/reel")
         }
 
         
